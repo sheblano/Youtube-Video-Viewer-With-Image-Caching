eb478a23c4d1cce7b4679a7e902d0ff98b7126d9
Mohamed Shebl <mohamed_shebl93@hotmail.com>
2017-07-29 04:17:33 +0200

    trying to add resources for the project icon and splash

--- typings/cordova-typings.d.ts
+++ typings/cordova-typings.d.ts
@@ -5,3 +5,7 @@
 /// <reference path="../.vscode/typings/cordova/plugins/Splashscreen.d.ts"/>
 /// <reference path="../.vscode/typings/cordova/plugins/FileSystem.d.ts"/>
 /// <reference path="../.vscode/typings/cordova/plugins/FileTransfer.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/cordova.d.ts"/>
+/// <reference path="../.vscode/typings/ionic/ionic.d.ts"/>
+/// <reference path="../.vscode/typings/angularjs/angular.d.ts"/>
+/// <reference path="../.vscode/typings/jquery/jquery.d.ts"/>
